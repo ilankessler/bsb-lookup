31b5e8038dbc7673c6485c11525cab09681a7fad
Ilan <ilan@refundid.com>
2024-10-10 16:58:31 +1100

    chore: retired ftp fix 2.0

--- .github/workflows/scripts/csv_update.py
+++ .github/workflows/scripts/csv_update.py
@@ -5,10 +5,11 @@ import os
 import urllib.parse
 
 _STATIC_BSB_DIRECTORY_URL = "https://bsb.auspaynet.com.au/Public/BSB_DB.NSF/getBSBFullCSV?OpenAgent"
+_STATIC_KEY_TO_ABBREVIATION_URL = "https://bsb.auspaynet.com.au/Public/BSB_DB.NSF/getKeytoACSV?OpenAgent"
 
 
-def _update_bsb_directory_file() -> str:
-    parsed_url = urllib.parse.urlparse(_STATIC_BSB_DIRECTORY_URL)
+def _update_file(static_url: str, dest: str) -> str:
+    parsed_url = urllib.parse.urlparse(static_url)
 
     conn = http.client.HTTPSConnection(parsed_url.netloc)
 
@@ -31,13 +32,21 @@ def _update_bsb_directory_file() -> str:
 
     file_bytes = response.read()
 
-    with open("./src/bsb/static/bsb_directory.csv", "wb") as f:
+    with open(dest, "wb") as f:
         f.write(file_bytes)
 
     return file_name
 
 
 if __name__ == "__main__":
-    directory_file_name: str = _update_bsb_directory_file()
+    prefix_file_name = _update_file(
+        static_url=_STATIC_KEY_TO_ABBREVIATION_URL, dest="./src/bsb/static/bsb_prefix_rules.csv"
+    )
+
+    directory_file_name: str = _update_file(
+        static_url=_STATIC_BSB_DIRECTORY_URL, dest="./src/bsb/static/bsb_directory.csv"
+    )
+
     with open(os.environ["GITHUB_OUTPUT"], "a") as output:
+        print(f"bsb_prefix_file={prefix_file_name}", file=output)
         print(f"bsb_directory_file={directory_file_name}", file=output)
